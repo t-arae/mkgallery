1f7467a484a014d8b28b38a6d4038519b2153083
t-arae <aroearue@gmail.com>
2024-10-16 11:44:25 +0900

    feat: add new gallery types

--- _extensions/mkgallery/mkgallery.lua
+++ _extensions/mkgallery/mkgallery.lua
@@ -1,12 +1,60 @@
-return {
-  ['mkgallery'] = function(args, kwargs, meta) 
+
+function pairsByKeys (t, f)
+  local a = {}
+  for n in pairs(t) do table.insert(a, n) end
+  table.sort(a, f)
+  local i = 0      -- iterator variable
+  local iter = function ()   -- iterator function
+    i = i + 1
+    if a[i] == nil then return nil
+    else return a[i], t[a[i]]
+    end
+  end
+  return iter
+end
+
+function set_kwargs(kwargs, logger)
+  local search_ext = pandoc.utils.stringify(kwargs["ext"])
+  search_ext = search_ext ~= "" and search_ext or 'png,jpeg,jpg'
+  logger("image ext: " .. search_ext)
+
+  local group = pandoc.utils.stringify(kwargs["group"])
+  group = group ~= "" and group or 'all'
+  logger("group: " .. group)
+
+  local min_width = pandoc.utils.stringify(kwargs["min-width"])
+  --min_width = min_width ~= "" and min_width or '240px'
+  min_width = min_width ~= "" and min_width or '200px'
+  logger("min_width: " .. min_width)
+
+  local hd_lev = pandoc.utils.stringify(kwargs["hd_lev"])
+  hd_lev = hd_lev ~= "" and (hd_lev + 0) or 2
+  logger("header level: " .. hd_lev)
+
+  return search_ext, group, min_width, hd_lev
+end
+
+function set_args(args, wd, logger)
+    local len_args = #args
+
+    local dir = ""
+    if len_args > 0 then
+      dir = pandoc.path.make_relative(args[1], wd, true)
+    end
+    logger("image dir: " .. dir)
+
+    local gallery_type = ""
+    if len_args > 1 then
+      gallery_type = pandoc.utils.stringify(args[2])
+    end
+    gallery_type = gallery_type ~= "" and gallery_type or 'scroll'
+    logger("gallery_type: " .. gallery_type)
+
+    return dir, gallery_type
+end
+
+function search_img_files(dir, search_ext)
     local lpeg = require"lpeg"
-    local wd = pandoc.system.get_working_directory()
-
-    local search_ext = pandoc.utils.stringify(kwargs["ext"])
-    search_ext = search_ext ~= "" and search_ext or 'png,jpeg,jpg'
-    print("image ext: " .. search_ext)
-
     -- Make file extension pattern
     local i = true
     for n in search_ext:gmatch("[%a%d]+") do
@@ -19,14 +67,6 @@ return {
     end 
     local cap_ext = lpeg.C(pat_ext)
     local pat = "." * cap_ext * -1
-
-    local hd_lev = pandoc.utils.stringify(kwargs["hd_lev"])
-    hd_lev = hd_lev ~= "" and (hd_lev + 0) or 2
-    print("header level: " .. hd_lev)
-
-    local dir = pandoc.path.make_relative(args[1], wd, true)
-    print("working dir: " .. wd)
-    print("image dir: " .. dir)
 
     local files = {}
     for _, v in ipairs(pandoc.system.list_directory(dir)) do
@@ -41,19 +81,96 @@ return {
       end
     end
 
-    local function insert_image_blocks(images)
-      local blocks = {}
-      for fname, img_path in pairs(images) do
-        print(fname)
-        local image_block = pandoc.Div({
-          pandoc.Header(hd_lev, fname, {id = fname}),
-          pandoc.Image({}, img_path, fname, {style = "width: 40%", class = "lightbox"})
-        })
-        table.insert(blocks, image_block)
+    return files
+end
+
+function build_img_block_wrap(images, group_id, min_width, hd_lev)
+  local st = "flex-grow: 1; min-width: " .. min_width .. "; max-width: calc(30% - 20px);"
+  local blocks = {}
+  for fname, img_path in pairsByKeys(images) do
+    local image_block = pandoc.Div({
+      pandoc.Header(hd_lev, fname, {id = fname}),
+      pandoc.Div(
+          pandoc.Image({}, img_path, fname, {class = "lightbox", group = group_id})
+        )},
+      {style = st}
+    )
+    table.insert(blocks, image_block)
+  end
+  return pandoc.Div(blocks, {
+    style = "display: flex; flex-wrap: wrap; gap: 20px; overflow-x: auto;"
+  })
+end
+
+local function build_img_block_scroll(images, group_id, min_width, hd_lev)
+  local st = "flex-grow: 1; min-width: " .. min_width .. "; max-width: calc(50% - 20px);"
+  local blocks = {}
+  for fname, img_path in pairsByKeys(images) do
+    local image_block = pandoc.Div({
+      pandoc.Header(hd_lev, fname, {id = fname}),
+      pandoc.Div(
+        pandoc.Image({}, img_path, fname, {
+          class = "lightbox",
+          group = group_id
+        }),
+        {style = st}
+      )
+    })
+    table.insert(blocks, image_block)
+  end
+  return pandoc.Div(blocks, {
+    style = "display: flex; gap: 20px; overflow-x: auto;"
+  })
+end
+
+local function build_img_block_list(images, group_id, min_width, hd_lev)
+  local st = "min-width: " .. min_width .. "; max-width: calc(50% - 20px);"
+  local blocks = {}
+  for fname, img_path in pairsByKeys(images) do
+    local image_block = pandoc.Div({
+      pandoc.Header(hd_lev, fname, {id = fname}),
+      pandoc.Div(
+        pandoc.Image({}, img_path, fname, {
+          class = "lightbox",
+          group = group_id
+        }),
+        {style = st}
+      )
+    })
+    table.insert(blocks, image_block)
+  end
+  return pandoc.Div(blocks, {
+    style = "overflow-x: auto;"
+  })
+end
+
+return {
+  ['mkgallery'] = function(args, kwargs, meta) 
+    
+    local logger = function(x)
+      if false then
+        print(x)
       end
-      return blocks
     end
 
-    return insert_image_blocks(files)
+    local wd = pandoc.system.get_working_directory()
+    logger("working dir: " .. wd)
+
+    -- keyward args
+    local search_ext, group_id, min_width, hd_lev = set_kwargs(kwargs, logger)
+    -- args
+    local dir, gallery_type = set_args(args, wd, logger)
+
+    local files = search_img_files(dir, search_ext)
+
+    if gallery_type == "scroll" then
+      return build_img_block_scroll(files, group_id, min_width, hd_lev)
+    elseif gallery_type == "wrap" then
+      return build_img_block_wrap(files, group_id, min_width, hd_lev)
+    elseif gallery_type == "list" then
+      return build_img_block_list(files, group_id, min_width, hd_lev)
+    else
+      error()
+    end
   end
 }
